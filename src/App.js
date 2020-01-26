5216cdd3f977db5b11f0a383d82ef7150bbff920
shishuwu <shuwu.shi@gmail.com>
2020-01-26 19:17:27 +0800

    Add basic components

--- src/App.js
+++ src/App.js
@@ -2,23 +2,74 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const quotes = [
+  {
+    author: 'Jason',
+    text: 'Hello world'
+  },
+  {
+    author: 'Maggie',
+    text: 'Hey there'
+  }
+]
+
+class QuoteArea extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: 'abc',
+      author: 'jason'
+    }
+  }
+
+  render() {
+    return (
+      <div id="quote-area">
+        <div id="text">{this.state.text}</div>
+        <div id="author">{this.state.author}</div>
+      </div>
+    );
+  }
+}
+
+class ButtonArea extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div id="button-area">
+        <button id="tweet-quote">tweet</button>
+        <button id="new-quote">New quote</button>
+      </div>
+    );
+  }
+}
+
+
+
+class QuoteBox extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div id="quote-box">
+        <QuoteArea />
+        <ButtonArea />
+      </div>
+    );
+  }
+}
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Random Quote Machine</h3>
+      <QuoteBox />
     </div>
   );
 }
