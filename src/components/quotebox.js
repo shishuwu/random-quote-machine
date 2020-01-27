d2a25dea1272f82f4c2f6ae248983ed2ecd5a360
shishuwu <shuwu.shi@gmail.com>
2020-01-27 17:43:16 +0800

    twitter

--- src/components/quotebox.js
+++ src/components/quotebox.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { randomA } from '../actions/actions';
-import { quotes } from '../constant/constant';
-
+import { quotes, Quote } from '../constant/constant';
 
 function QuoteArea() {
     const randomR = useSelector(state => state.randomR);
-
+    
     return (
         <div id="quote-area">
             <div id="text">{quotes[randomR].author}</div>
@@ -22,7 +21,7 @@ function ButtonArea() {
 
     return (
         <div id="button-area">
-            <a class="button" id="tweet-quote" title="Tweet this quote!" target="_blank" href={"https://twitter.com/intent/tweet?text=" + quotes[randomR].author + quotes[randomR].text}>tweet</a>
+            <a id="tweet-quote" title="Tweet this quote!" target="_blank" href={"https://twitter.com/intent/tweet?text=" + quotes[randomR].toString()}>tweet</a>
             <button id="new-quote" onClick={() => { dispatch(randomA(quotes.length)) }}>New quote</button>
         </div >
     )
--- src/constant/constant.js
+++ src/constant/constant.js
@@ -1,15 +1,17 @@
 // CONSTANT
-export const quotes = [
-    {
-        author: 'Jason',
-        text: 'Hello world'
-    },
-    {
-        author: 'Maggie',
-        text: 'Hey there'
-    },
-    {
-        author: 'Ben',
-        text: ':-) is the best cure'
+export class Quote {
+    constructor(author, text) {
+        this.author = author;
+        this.text = text;
     }
+
+    toString() {
+        return "\"" + this.text + "\" " + this.author + " \#quotes"
+    }
+}
+
+export const quotes = [
+    new Quote('Jason', "Hello world"),
+    new Quote('Maggie', 'Hey there'),
+    new Quote('Ben', 'Smile is the best cure ;-)')
 ]
