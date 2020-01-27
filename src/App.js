f660600d2f80d20be7df1466be47e1f15fc42b3c
shishuwu <shuwu.shi@gmail.com>
2020-01-27 16:34:30 +0800

    refine code

--- src/App.js
+++ src/App.js
@@ -22,19 +22,16 @@ class QuoteArea extends React.Component {
   }
 }
 
-class ButtonArea extends React.Component {
-  constructor(props) {
-    super(props);
-  }
 
-  render() {
-    return (
-      <div id="button-area">
-        <button id="tweet-quote">tweet</button>
-        <button id="new-quote" onClick={() => { }}>New quote</button>
-      </div>
-    );
-  }
+function ButtonArea() {
+  const dispatch = useDispatch();
+
+  return (
+    <div id="button-area">
+      <button id="tweet-quote">tweet</button>
+      <button id="new-quote" onClick={() => { dispatch(random(100))}}>New quote</button>
+    </div>
+  )
 }
 
 
@@ -57,13 +54,11 @@ class QuoteBox extends React.Component {
 
 function App() {
   const randomR = useSelector(state => state.randomR);
-  const dispatch = useDispatch();
-  
+ 
   return (
     <div className="App">
       <h3>Random Quote Machine</h3>
       <h4>Random number: {randomR} </h4>
-      <button onClick={() => { dispatch(random(100)) }}>click</button>
       <QuoteBox />
     </div>
   );
