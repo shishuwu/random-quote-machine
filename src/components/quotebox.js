002def3e378681bea587b8eecb8c988ee3852b0a
shishuwu <shuwu.shi@gmail.com>
2020-01-27 17:28:52 +0800

    add twitter function

--- src/components/quotebox.js
+++ src/components/quotebox.js
@@ -6,33 +6,35 @@ import { quotes } from '../constant/constant';
 
 function QuoteArea() {
     const randomR = useSelector(state => state.randomR);
-  
-    return (
-      <div id="quote-area">
-        <div id="text">{quotes[randomR].author}</div>
-        <div id="author">{quotes[randomR].text}</div>
-      </div>
-    )
-  }
-  
-  function ButtonArea() {
-    const dispatch = useDispatch();
-  
-    return (
-      <div id="button-area">
-        <button id="tweet-quote">tweet</button>
-        <button id="new-quote" onClick={() => { dispatch(randomA(quotes.length)) }}>New quote</button>
-      </div>
-    )
-  }
-  
-  function QuoteBox() {
-    return (
-      <div id="quote-box">
-        <QuoteArea />
-        <ButtonArea />
-      </div>
-    )
-  }
 
-  export default QuoteBox;
+    return (
+        <div id="quote-area">
+            <div id="text">{quotes[randomR].author}</div>
+            <div id="author">{quotes[randomR].text}</div>
+        </div>
+    )
+}
+
+function ButtonArea() {
+    const dispatch = useDispatch();
+
+    const randomR = useSelector(state => state.randomR);
+
+    return (
+        <div id="button-area">
+            <a class="button" id="tweet-quote" title="Tweet this quote!" target="_blank" href={"https://twitter.com/intent/tweet?text=" + quotes[randomR].author + quotes[randomR].text}>tweet</a>
+            <button id="new-quote" onClick={() => { dispatch(randomA(quotes.length)) }}>New quote</button>
+        </div >
+    )
+}
+
+function QuoteBox() {
+    return (
+        <div id="quote-box">
+            <QuoteArea />
+            <ButtonArea />
+        </div>
+    )
+}
+
+export default QuoteBox;
