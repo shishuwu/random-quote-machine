bdaba3cde2471d4118800559cea9f688e8a71a16
shishuwu <shuwu.shi@gmail.com>
2020-01-27 18:20:32 +0800

    add boostrap

--- src/components/quotebox.js
+++ src/components/quotebox.js
@@ -2,14 +2,15 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { randomA } from '../actions/actions';
 import { quotes, Quote } from '../constant/constant';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function QuoteArea() {
     const randomR = useSelector(state => state.randomR);
-    
+
     return (
         <div id="quote-area">
-            <div id="text">{quotes[randomR].author}</div>
             <div id="author">{quotes[randomR].text}</div>
+            <div id="text">-{quotes[randomR].author}</div>
         </div>
     )
 }
@@ -21,8 +22,8 @@ function ButtonArea() {
 
     return (
         <div id="button-area">
-            <a id="tweet-quote" title="Tweet this quote!" target="_blank" href={"https://twitter.com/intent/tweet?text=" + quotes[randomR].toString()}>tweet</a>
-            <button id="new-quote" onClick={() => { dispatch(randomA(quotes.length)) }}>New quote</button>
+            <a class="btn btn-warning" role="button" id="tweet-quote" title="Tweet this quote!" target="_blank" href={"https://twitter.com/intent/tweet?text=" + quotes[randomR].toString()}>tweet</a>
+            <button class="btn btn-info" id="new-quote" onClick={() => { dispatch(randomA(quotes.length)) }}>New quote</button>
         </div >
     )
 }
